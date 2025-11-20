356ffa7c571a621f2a8e87ed158cd30f87ce4565
srdoppelganger <arthur.albuquerque2004@gmail.com>
2025-11-20 18:31:50 -0300

    added more sprites

--- Scripts/sala_quiz.gd
+++ Scripts/sala_quiz.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var king: AnimatedSprite2D = $CanvasLayer/king
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,11 @@ func _ready() -> void:
 func _on_voltar_pressed() -> void:
 	#add som de "aceite" aqui
 	get_tree().change_scene_to_file("res://Scenes/sala_principal.tscn");
+
+
+func _on_area_2d_mouse_entered() -> void:
+	king.play("active");
+
+
+func _on_area_2d_mouse_exited() -> void:
+	king.play("default");
