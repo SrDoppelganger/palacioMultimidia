525526237eda8d30f113a7ed9d3cb25674578e0b
srdoppelganger <arthur.albuquerque2004@gmail.com>
2025-11-24 15:56:53 -0300

    fixed text boxes

--- Scripts/sala_d_1.gd
+++ Scripts/sala_d_1.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var textbox: CanvasLayer = $Textbox
+@onready var exit: TextureButton = $Textbox/exit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +13,19 @@ func _on_continuar_pressed() -> void:
 
 func _on_voltar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/sala_principal.tscn");
+
+func show_info(info:String):
+	textbox.set_text("haiiiiiii");
+	textbox.show();
+	textbox.show_text();
+	exit.show();
+
+func _on_area_leitura_1_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			print("clicado")
+			show_info("leitura 1");
+
+func _on_exit_pressed() -> void:
+	textbox.hide_text();
+	exit.hide();
--- Scripts/textbox.gd
+++ Scripts/textbox.gd
@@ -2,23 +2,14 @@ extends CanvasLayer
 
 @onready var textbox_container: MarginContainer = $"Layout/Textbox container";
 @onready var label: Label = $"Layout/Textbox container/MarginContainer/HBoxContainer/Label";
-@onready var exit: TextureButton = $Layout/exit
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	set_text("lorem ipsum");
 	
 func hide_text():
 	label.text = "";
 	textbox_container.hide();
-	exit.hide();
-	exit.disabled = true;
 
 func show_text():
 	textbox_container.show();
 
 func set_text(text:String):
 	label.text = text
-
-func _on_exit_pressed() -> void:
-	hide_text();
