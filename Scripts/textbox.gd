f835ddb8c594228a984d2d20d38678c2495bc62b
srdoppelganger <arthur.albuquerque2004@gmail.com>
2025-11-24 15:21:56 -0300

    finished basic textbox

--- Scripts/textbox.gd
+++ Scripts/textbox.gd
@@ -1,13 +1,24 @@
 extends CanvasLayer
 
-@onready var textbox_container: MarginContainer = $"Textbox container"
-@onready var label: Label = $"Textbox container/MarginContainer/HBoxContainer/Label"
+@onready var textbox_container: MarginContainer = $"Layout/Textbox container";
+@onready var label: Label = $"Layout/Textbox container/MarginContainer/HBoxContainer/Label";
+@onready var exit: TextureButton = $Layout/exit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	hide_text();
+	set_text("lorem ipsum");
 	
 func hide_text():
 	label.text = "";
 	textbox_container.hide();
-	
+	exit.hide();
+	exit.disabled = true;
+
+func show_text():
+	textbox_container.show();
+
+func set_text(text:String):
+	label.text = text
+
+func _on_exit_pressed() -> void:
+	hide_text();
